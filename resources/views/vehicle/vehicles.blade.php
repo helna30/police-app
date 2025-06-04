9ac467b91cca44d2cff7f88194ffe6a73a765acc
helna30 <nahelseptia@gmail.com>
2025-06-04 14:08:47 +0700

    update

--- resources/views/vehicle/vehicles.blade.php
+++ resources/views/vehicle/vehicles.blade.php
@@ -1,137 +1,151 @@
 @extends('components.master')
 
 @section('content')
-<div class="container-fluid pt-2 d-flex justify-content-center">
-    <div class="card shadow-sm w-100" style="max-width: 900px;">
-        <div class="card-header">
-            <div class="d-flex justify-content-between align-items-center">
-                <h4 class="card-title mb-0">Daftar Kendaraan</h4>
+    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ url('/dashboard') }}">Dashboard</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Vehicles</li>
+        </ol>
+    </nav>
 
-                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addVehicleModal">
-                    <i class="fas fa-plus"></i> Tambah Kendaraan
-                </button>
-            </div>
-        </div>
-        <div class="card-body">
-            <table class="table table-striped table-hover" id = "vehiclesTable">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Plat Nomor</th>
-                        <th>Tipe</th>
-                        <th>Merk</th>
-                        <th>Warna</th>
-                        <th>Stolen</th>
-                        <th>Aksi</th>
-                    </tr>
-                </thead>
-                <tbody id="VehicleTableBody">
-                </tbody>
-            </table>
+    <div class="d-flex flex-row-reverse">
+        <div class="p-2">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createVehicleModal"
+                id="addVehicleBtn">
+                Add Vehicle
+            </button>
         </div>
     </div>
 
-    @foreach ($vehicles as $index => $vehicle)
-    <!-- Modal Edit Kendaraan -->
-    <div class="modal fade" id="editVehicleModal{{ $vehicle->id }}" tabindex="-1" aria-labelledby="editVehicleModalLabel" aria-hidden="true">
+    <table class="table table-striped" id="vehiclesTable">
+        <thead class="bg-primary text-white">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">License Plate</th>
+                <th scope="col">Type</th>
+                <th scope="col">Brand</th>
+                <th scope="col">Color</th>
+                <th scope="col">Is Stolen?</th>
+                <th scope="col">Option</th>
+            </tr>
+        </thead>
+        <tbody id="vehiclesTableBody">
+
+        </tbody>
+    </table>
+
+    <div class="modal fade" id="createVehicleModal" tabindex="-1" aria-labelledby="createVehicleModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="editVehicleModalLabel">Edit Kendaraan</h5>
+                    <h5 class="modal-title" id="createVehicleModalLabel">Create New Vehicle</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form>
+                    <form id="createVehicleForm">
                         <div class="mb-3">
-                            <label for="license_plate" class="form-label">Plat Nomor</label>
-                            <input type="text" class="form-control" id="license_plate" name="license_plate" value="{{ $vehicle->license_plate }}">
+                            <label for="createLicensePlate" class="form-label">License Plate</label>
+                            <input type="text" class="form-control" id="createLicensePlate">
+                            <small id="createLicensePlateError" class="text-danger"></small>
                         </div>
                         <div class="mb-3">
-                            <label for="type" class="form-label">Tipe</label>
-                            <input type="text" class="form-control" id="type" name="type" value="{{ $vehicle->type }}">
+                            <label for="createType" class="form-label">Type</label>
+                            <input type="text" class="form-control" id="createType">
+                            <small id="createTypeError" class="text-danger"></small>
                         </div>
                         <div class="mb-3">
-                            <label for="brand" class="form-label">Merk</label>
-                            <input type="text" class="form-control" id="brand" name="brand" value="{{ $vehicle->brand }}">
+                            <label for="createBrand" class="form-label">Brand</label>
+                            <input type="text" class="form-control" id="createBrand">
+                            <small id="createBrandError" class="text-danger"></small>
                         </div>
                         <div class="mb-3">
-                            <label for="color" class="form-label">Warna</label>
-                            <input type="text" class="form-control" id="color" name="color" value="{{ $vehicle->color }}">
+                            <label for="createColor" class="form-label">Color</label>
+                            <input type="text" class="form-control" id="createColor">
+                            <small id="createColorError" class="text-danger"></small>
                         </div>
                         <div class="mb-3">
-                            <label for="is_stolen" class="form-label">Stolen</label>
-                            <div class="d-flex">
-                                <div class="form-check me-3">
-                                    <input class="form-check-input" type="radio" name="is_stolen" id="is_stolen_yes{{ $vehicle->id }}" value="1" {{ $vehicle->is_stolen ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="is_stolen_yes{{ $vehicle->id }}">Yes</label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="is_stolen" id="is_stolen_no{{ $vehicle->id }}" value="0" {{ !$vehicle->is_stolen ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="is_stolen_no{{ $vehicle->id }}">No</label>
-                                </div>
+                            <label class="form-label">Is Stolen?</label>
+                            <br>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="createIsStolen" id="createIsStolenNo"
+                                    value="0" checked>
+                                <label class="form-check-label" for="createIsStolenNo">No</label>
                             </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="createIsStolen" id="createIsStolenYes"
+                                    value="1">
+                                <label class="form-check-label" for="createIsStolenYes">Yes</label>
+                            </div>
+                            <small id="createIsStolenError" class="text-danger"></small>
                         </div>
-                        <button type="submit" class="btn btn-primary">Update</button>
                     </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" form="createVehicleForm">Save Vehicle</button>
                 </div>
             </div>
         </div>
     </div>
-    @endforeach
 
-    <!-- Modal Tambah Kendaraan -->
-    <div class="modal fade" id="addVehicleModal" tabindex="-1" aria-labelledby="addVehicleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="editVehicleModal" tabindex="-1" aria-labelledby="editVehicleModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="addVehicleModalLabel">Tambah Kendaraan Baru</h5>
+                    <h5 class="modal-title" id="editVehicleModalLabel">Edit Vehicle</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <form id="vehicleForm">
+
+                <form id="editVehicleForm">
+                    <div class="modal-body">
+                        <input type="hidden" id="editVehicleId">
+
                         <div class="mb-3">
-                            <label for="license_plate" class="form-label">Plat Nomor</label>
-                            <input type="text" class="form-control" id="license_plate" name="license_plate">
-                            <small id="license_plateError" class="text-danger"></small>
+                            <label for="editLicensePlate" class="form-label">License Plate</label>
+                            <input type="text" class="form-control" id="editLicensePlate">
+                            <small id="editLicensePlateError" class="text-danger"></small>
                         </div>
                         <div class="mb-3">
-                            <label for="type" class="form-label">Tipe</label>
-                            <input type="text" class="form-control" id="type" name="type">
-                            <small id="typeError" class="text-danger"></small>
+                            <label for="editType" class="form-label">Type</label>
+                            <input type="text" class="form-control" id="editType">
+                             <small id="editTypeError" class="text-danger"></small>
                         </div>
                         <div class="mb-3">
-                            <label for="brand" class="form-label">Merk</label>
-                            <input type="text" class="form-control" id="brand" name="brand">
-                            <small id="brandError" class="text-danger"></small>
+                            <label for="editBrand" class="form-label">Brand</label>
+                            <input type="text" class="form-control" id="editBrand">
+                             <small id="editBrandError" class="text-danger"></small>
                         </div>
                         <div class="mb-3">
-                            <label for="color" class="form-label">Warna</label>
-                            <input type="text" class="form-control" id="color" name="color">
-                            <small id="colorError" class="text-danger"></small>
+                            <label for="editColor" class="form-label">Color</label>
+                            <input type="text" class="form-control" id="editColor">
+                             <small id="editColorError" class="text-danger"></small>
                         </div>
                         <div class="mb-3">
-                            <label for="is_stolen" class="form-label">Stolen</label>
-                            <div class="d-flex">
-                                <div class="form-check me-3">
-                                    <input class="form-check-input" type="radio" name="is_stolen" id="is_stolen_yes" value="1">
-                                    <label class="form-check-label" for="is_stolen_yes">Yes</label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="is_stolen" id="is_stolen_no" value="0" checked>
-                                    <label class="form-check-label" for="is_stolen_no">No</label>
-                                </div>
+                            <label for="createIsStolen" class="form-label">Is Stolen?</label>
+                            <br>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" name="is_stolen" id="editIsStolenYes"
+                                    value="0">
+                                <label class="form-check-label" for="inlineRadio1">Yes</label>
                             </div>
-                            <small id="is_stolenError" class="text-danger"></small>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" name="is_stolen" id="editIsStolenNo"
+                                    value="1">
+                                <label class="form-check-label" for="inlineRadio2">No</label>
+                            </div>
+                            <small id="editIsStolenError" class="text-danger"></small>
                         </div>
 
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                        <button type="submit" class="btn btn-primary">Simpan</button>
-                    </form>
-                </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Update Vehicle</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
-</div>
-
 @endsection
